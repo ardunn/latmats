e57b9dba955a3513712bf2c78c8edc93272dd8d0
Amalie Trewartha <amalie.trewartha@gmail.com>
2020-01-29 16:03:01 -0800

    Added keras_utils

---
+++ keras_utils.py
@@ -0,0 +1,92 @@
+import tensorflow as tf
+import numpy as np
+import re
+
+elements = ["H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na", "Mg", "Pa", "Al", "Np", "Am", "Si", "P", "S", "Cl", "Ar", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd",
+            "In", "Sn", "Sb", "Te", "I", "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd", "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb", "Lu", "Hf", "Ta", "W", "Re", "Os", "Ir", "Pt", "Au", "Hg", "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th", "U", "Pu", "Cm", "Bk", "Cf", "Es", "Fm", "Md", "No", "Lr"]
+
+formulare = re.compile(r'([A-Z][a-z]*)(\d*)')
+
+
+def parse_formula(formula):
+    pairs = formulare.findall(formula)
+    length = sum((len(p[0]) + len(p[1]) for p in pairs))
+    assert length == len(formula)
+    formula_dict = defaultdict(int)
+    for el, sub in pairs:
+        formula_dict[el] += float(sub) if sub else 1
+
+    keys = formula_dict.keys()
+    values = formula_dict.values()
+    total = float(sum(values))
+    formula_vec = np.array((len(elements)))
+    for k in keys:
+        formula_vec[elements.index(k)] = formula_dict[k] / total
+    return formula_vec
+
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+
+def is_material(word, material2index):
+    try:
+        material2index[word]
+        return 1
+    except IndexError:
+        return 0
+
+
+def fancy_sentence_sampler(sentence, window_size, material2index=None):
+    sentence_length = len(sentence)
+    if material2index is not None:
+        mask = softmax([is_material(w, material2index) for w in sentence])
+        indices = np.arange(0, sentence_length)
+        centre = np.random.choice(indices, p=mask)
+    else:
+        centre = np.random.randint(0, sentence_length)
+
+    if centre <= window_size:
+        min_value = 0
+    else:
+        min_value = centre - window_size
+    if centre + window_size + 1 > sentence_length:
+        max_value = sentence_length
+    else:
+        max_value = centre + window_size + 1
+
+    indices = np.arange(min_value, max_value)
+    probs = list(reversed(np.arange(1, centre - min_value + 1))) + \
+        [0] + list(np.arange(1, max_value - centre))
+
+    probs = softmax(probs)
+
+    index = np.random.choice(indices, p=probs)
+
+    context = sentence[index]
+    target = sentence[centre]
+    return target, context
+
+
+def example_generator_not_material(corpus, window_size, word2index):
+    n_examples = len(corpus)
+
+    while True:
+        abstract = corpus[np.random.randint(0, n_examples)]
+
+        target, context = fancy_sentence_sampler(abstract, window_size)
+
+        # Dummy output
+        yield (word2index[target], word2index[context]), 0
+
+
+def example_generator_material(corpus, window_size, word2index, material2index):
+    n_examples = len(corpus)
+
+    abstract = corpus[np.random.randint(0, n_examples)]
+
+    target, context = fancy_sentence_sampler_material(
+        abstract, window_size, material2index=material2index)
+
+    yield parse_formula(target), word2index(context)
